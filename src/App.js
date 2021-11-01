6daefe9ccff7e3ac29986250db339d676097c15a
OksanaKis <oksanakisetoja@gmail.com>
2021-11-01 14:16:01 +0200

    creating useEffect hook, checkingLines and isComputerTurn

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import React from 'react';
 import Board from './components/Board';
 import Square from './components/Square';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const defaultSquares = () => (new Array(9)).fill(null);
+
+const lines = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6],
+];
 
 function App() {
   const [squares, setSquares] = useState(defaultSquares());
   const [winner,setWinner] = useState(null);
 
+  useEffect (() => {
+    const isComputerTurn = squares.filter(elemSquares => elemSquares !== null).length % 2 === 1;
+  }, [squares]);
 
   const handleClick = (index) => {
     if ((squares[index] !== null) || winner ) return;
@@ -21,7 +30,7 @@ function App() {
     }
   }
 
-  
+
   return (
     <main>
       <div className='header'>Tic tac toe</div>
