a1c2440b152b422b2e64fd5a592befc403739b8a
OksanaKis <oksanakisetoja@gmail.com>
2021-11-01 14:10:37 +0200

    import Square and adding  handleClick function, and setting 'x'.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
 import React from 'react';
 import Board from './components/Board';
+import Square from './components/Square';
+import { useState } from 'react';
+
+const defaultSquares = () => (new Array(9)).fill(null);
 
 function App() {
+  const [squares, setSquares] = useState(defaultSquares());
+  const [winner,setWinner] = useState(null);
+
+
+  const handleClick = (index) => {
+    if ((squares[index] !== null) || winner ) return;
+    const isPlayerTurn = squares.filter(elemSquares => elemSquares !== null).length % 2 === 0;
+    if (isPlayerTurn) {
+       let newSquares = squares;
+       newSquares[index] = 'x';
+       setSquares([...newSquares]);
+    }
+  }
+
+  
   return (
     <main>
       <div className='header'>Tic tac toe</div>
       <Board>
-        
+      {squares.map((square,index) =>
+          <Square
+            key={index}
+            x={square==='x'?1:0}
+            o={square==='o'?1:0}
+            onClick={() => handleClick(index)} />
+        )}
       </Board>
     </main>
   );
