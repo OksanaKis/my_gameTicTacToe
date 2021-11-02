271e1033b34f9be0eee59581c28456205dc5ee21
OksanaKis <oksanakisetoja@gmail.com>
2021-11-02 11:04:24 +0200

    adding hard level button

--- src/App.js
+++ src/App.js
@@ -131,6 +131,8 @@ if (isComputerTurn && emptyIndexes.length > 0) {
       <div className="button_div">
       <button className="button_easy" onClick={changeLevel}>Easy level</button>
       <button className="button_normal" onClick={changeLevel}>Normal level</button>
+      <button className="button hard" 
+      onClick={changeLevel}>Hard</button>
       <button className="button_reset" 
       onClick={resetClick}>Reset</button>
       </div>
